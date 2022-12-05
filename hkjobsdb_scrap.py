8bf154e8f6958e4a2ba05521658cf1018575fe59
aster <asterfung24@gmail.com>
2022-12-05 23:02:26 +0800

    completed: scrape additional info: career level etc. function <scrap_additional_info_as_dict()>

--- hkjobsdb_scrap.py
+++ hkjobsdb_scrap.py
@@ -20,7 +20,7 @@ LOAD_SEC_PER_JOB = 5       # increase if internet is slow
 TODAY_STR = str(date.today())
 
 
-def generate_list_of_job_pages (element_driver):
+def generate_list_of_job_pages(element_driver):
     # generate page list
     time.sleep(LOAD_SEC_PER_JOB)
     last_page = element_driver.find_elements(By.XPATH, '//*[@id="pagination"]/option')[-1].text
@@ -39,18 +39,24 @@ def initialize_field_names():
                           'company',
                           'posted',
                           'district',
-                          'job_description'
+                          'job_description',
+                          'Career Level',
+                          'Years of Experience',
+                          'Company Website',
+                          'Qualification',
+                          'Job Type',
+                          'Job Functions'
                           ]
     return field_names_fscope
 
 
-def get_job_ad_data ():
+def get_job_ad_data():
     """
     :param: element containing a single complete job ad at the right panel
     :return: a list of elements containing job ad data by category: title, company, salary, posted_date, district, job description
     """
     # title, company
-    single_job_ad_data = []
+    single_job_ad_data_fscope = []
     time.sleep(LOAD_SEC_PER_JOB)
 
     try:
@@ -70,15 +76,14 @@ def get_job_ad_data ():
         blob = driver.find_elements(By.XPATH, '//span[@class ="sx2jih0 zcydq84u _18qlyvc0 _18qlyvc1x _18qlyvc1 _18qlyvca"]')
         bblob_1 = [blob[5].text, blob[6].text, blob[7].text]
         for i in bblob_1:
-            if 'posted' in i.lower() and len(i)<30:
+            if 'posted' in i.lower() and len(i) < 30:
                 posted_date = hkjobsdb_date_reformat.date_reformat(i)
-            elif 'hk' in i.lower() and len(i)<30:
+            elif 'hk' in i.lower() and len(i) < 30:
                 salary = i
-            elif len(i)<30:
+            elif len(i) < 30:
                 district = i
     except exceptions.NoSuchElementException:
         pass
-
 
     # job description
     try:
@@ -86,25 +91,49 @@ def get_job_ad_data ():
     except:
         job_description = 'job_description'
 
-    single_job_ad_data.append(title)
-    single_job_ad_data.append(salary)
-    single_job_ad_data.append(company)
-    single_job_ad_data.append(posted_date)
-    single_job_ad_data.append(district)
-    single_job_ad_data.append(job_description)
+    # get additional info about the job add
+    # separate function because of the html structure
 
-    '''
-    # test code do not delete
-    temp = driver.find_elements(By.XPATH, '//span[@class ="sx2jih0 zcydq84u _18qlyvc0 _18qlyvc1x _18qlyvc1 _18qlyvca"]')
-    k = 0
-    for i in temp:
-        s = i.text
-        single_job_ad_data.append(f'*{k}*\n'+s)
-        k += 1
-    '''
+    single_job_ad_data_fscope.append(title)
+    single_job_ad_data_fscope.append(salary)
+    single_job_ad_data_fscope.append(company)
+    single_job_ad_data_fscope.append(posted_date)
+    single_job_ad_data_fscope.append(district)
+    single_job_ad_data_fscope.append(job_description)
 
-    return single_job_ad_data
+    return single_job_ad_data_fscope
 
+
+def scrap_additional_info_as_dict():
+    """
+    :return: a dictionary of scrapped text. keys are Career Level, Years of Experience, Company Website, Qualification, Job Type, Job Functions
+    """
+    time.sleep(5)
+    # assume all data are missing
+    additional_columns_default = ['Career Level', 'Years of Experience', 'Company Website', 'Qualification', 'Job Type', 'Job Functions']
+    additional_values_default = ['career', 'yearsofexperience', 'companywebsite', 'qualification', 'jobtype', ]
+    additional_info_dict = dict(map(lambda i, j: (i, j), additional_columns_default, additional_values_default))
+    try:
+        additional_table = driver.find_element(By.XPATH, '//div[@class ="sx2jih0 zcydq856 zcydq8fe _17fduda27"]')
+        # a table containing 6 pieces of info
+        additional_info_raw = additional_table.text
+        additional_info_list = additional_info_raw.split('\n')
+        table_len = len(additional_info_list)
+        additional_columns = []
+        additional_values = []
+        for i in range(table_len):
+            if i % 2 == 0:
+                additional_columns.append(additional_info_list[i])
+            else:
+                additional_values.append(additional_info_list[i])
+                additional_info_dict = dict(map(lambda i, j: (i, j), additional_columns, additional_values))
+    except exceptions.NoSuchElementException:
+        print('failed to scrape additional info')
+        # the additional data remains the default values
+        # meaning they are missing
+        pass
+    return additional_info_dict
+    # always has 6 key/value pairs
 
 
 # setup driver
@@ -117,11 +146,11 @@ driver.get(WEBSITE+'1')
 
 search_results = generate_list_of_job_pages(driver)
 
-i = 1
+scrap_count = 1
 field_names = initialize_field_names()
-file_name = f'{TARGET_JOB}-{TODAY_STR}.csv'
+file_name = f'{TARGET_JOB}-{TODAY_STR}-1.csv'
 
-with open(file_name, 'w',encoding="utf-8", newline='') as fh:
+with open(file_name, 'w', encoding="utf-8", newline='') as fh:
     odictwriter = DictWriter(fh, fieldnames=field_names)
     odictwriter.writeheader()
 
@@ -138,7 +167,6 @@ for page in search_results:
     while scrolling:
 
         try:
-
             left_panel_job_link_list = driver.find_elements(By.XPATH, '//div[@class="sx2jih0 zcydq8n lmSnC_0"]')
 
             for job_link in left_panel_job_link_list:
@@ -153,31 +181,29 @@ for page in search_results:
                 # visit the right panel to access data of this job
                 single_job_ad_data = get_job_ad_data()
 
+                current_entry_part_1 = {'title': single_job_ad_data[0],
+                                        'salary': single_job_ad_data[1],
+                                        'company': single_job_ad_data[2],
+                                        'posted': single_job_ad_data[3],
+                                        'district': single_job_ad_data[4],
+                                        'job_description': single_job_ad_data[5]
+                                        }
+                # get dictionary
+                current_entry_part_2 = scrap_additional_info_as_dict()
+                print(current_entry_part_2)        # scaffold
 
-                current_entry = {   'title':single_job_ad_data[0],
-                                    'salary':single_job_ad_data[1],
-                                    'company': single_job_ad_data[2],
-                                    'posted':single_job_ad_data[3],
-                                    'district':single_job_ad_data[4],
-                                    'job_description':single_job_ad_data[5]
-                }
-                '''
-                jobs_title.append(single_job_ad_data[0])
-                jobs_company.append(single_job_ad_data[1])
-                jobs_salary.append(single_job_ad_data[2])
-                jobs_posted_date.append(single_job_ad_data[3])
-                jobs_district.append(single_job_ad_data[4])
-                jobs_job_description.append(single_job_ad_data[5])
-                '''
-                i += 1
-                # for item in single_job_ad_data:
-                #    print('~'+item)
-                print('scrapping job ad number: ', i)
+                print('scrapping job ad number: ', scrap_count)
+                scrap_count += 1
+
+                # merge two scraped data
+                current_entry_final = current_entry_part_1
+                current_entry_final.update(current_entry_part_2)
+                print('final')                       # scaffold
+                print(current_entry_final)           # scaffold
 
                 with open(file_name, 'a', encoding="utf-8", newline='') as fh:
                     odictwriter = DictWriter(fh, fieldnames=field_names)
-                    odictwriter.writerow(current_entry)
-
+                    odictwriter.writerow(current_entry_final)
 
                 current_reading_job_item += 1
                 if current_reading_job_item > max_job_per_page:
@@ -185,7 +211,7 @@ for page in search_results:
                     break
 
         except exceptions.StaleElementReferenceException:
-            print('stale')
+            print('end of page')
             # prevent the program from breaking when there is not more job ad to scrap on this single page
             break
     print('loading next page')
